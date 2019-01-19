57164f3d225423d22dff1abb65445b0d1cb5f902
Congthanh9768 <vancong1166@gmail.com>
2019-01-20 00:04:22 +0700

    Faction

--- Javacorebai4/src/bai7/Fraction.java
+++ Javacorebai4/src/bai7/Fraction.java
@@ -4,5 +4,91 @@
 package bai7;
 
 public class Fraction {
+    private int tu;
+    private int mau;
+    
+    
+    public Fraction(){
+        
+    }
+    
+    public Fraction(int tu,int mau){
+        this.tu = tu;
+        this.mau = mau;
+    }
+    
+    public int getTu(){
+        return tu;
+    }
+    
+    public void setTu(int tu){
+        this.tu = tu;
+    }
+    
+    public int getMau(){
+        return mau;
+    }
+    
+    public void setMau(int mau){
+        this.mau = mau;
+    }
+    
+    
+    public int FoundUTLN(int a ,int b){
+        while(a!=b){
+            if(a > b){
+                a -= b;
+            }
+            else{
+                b -= a;
+            }
+        }
+        return a;
+    }
+     public void toiGianFraction() {
+        int i = FoundUTLN(this.getTu(), this.getMau());
+        this.setTu(this.getTu() / i);
+        this.setMau(this.getMau() / i);
+    }
+    
+    public void  showFraction(){
+        System.out.println(FoundUTLN(getTu(), getMau())+"/"+FoundUTLN(getTu(),getTu()));
+    }
+    
+    
+     public void AddFraction(Fraction ps) {
+        int ts = this.getTu() * ps.getMau() + ps.getTu() * this.getMau();
+        int ms = this.getMau() * ps.getMau();
+        Fraction phanSoTong = new Fraction(ts, ms);
+        phanSoTong.toiGianFraction();
+        System.out.println("T?ng hai phân s? = " + phanSoTong.tu + "/" + phanSoTong.mau);
+    }
+      
+    public void SubFraction(Fraction ps) {
+        int ts = this.getTu() * ps.getMau() - ps.getTu() * this.getMau();
+        int ms = this.getMau() * ps.getMau();
+        Fraction phanSoHieu = new Fraction(ts, ms);
+        phanSoHieu.toiGianFraction();
+        System.out.println("Hi?u hai phân s? = " + phanSoHieu.tu + "/" + phanSoHieu.mau);
+    }
+     
+    public void MulFraction(Fraction ps) {
+        int ts = this.getTu() * ps.getTu();
+        int ms = this.getMau() * ps.getMau();
+        Fraction phanSoTich = new Fraction(ts, ms);
+        phanSoTich.toiGianFraction();
+        System.out.println("Tích hai phân s? = " + phanSoTich.tu + "/" + phanSoTich.mau);
+    }
+     
+    public void DivFraction(Fraction ps) {
+        int ts = this.getTu() * ps.getMau();
+        int ms = this.getMau() * ps.getTu();
+        Fraction phanSoThuong = new Fraction(ts, ms);
+        phanSoThuong.toiGianFraction();
+        System.out.println("Thuong hai phân s? = " + phanSoThuong.tu + "/" + phanSoThuong.mau);
+    }
+
+    
+    
     
 }
--- Javacorebai4/src/bai7/Main.java
+++ Javacorebai4/src/bai7/Main.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
 Xây dựng lớp Phân số (Fraction) có các thành phần sau:
 * Các thuộc tính: Tử số, mẫu số.
 * Hàm tạo không đối số và hai đối số để khởi tạo giá trị cho tử số và mẫu số.* Các phương thức setter và getter.
@@ -13,5 +13,14 @@ package bai7;
 
 
 public class Main {
-    
+     public static void main(String[] args) {
+        Fraction phanSo1=new Fraction(3,4);    // tạo phân số có tử = 16 và mẫu = 4
+        Fraction phanSo2=new Fraction(4,8); // tạo phân số có tử = 4 và mẫu = 8
+         
+        // gọi phương thức cộng, trừ, nhân, chia 2 phân số
+        phanSo1.AddFraction(phanSo2);
+        phanSo1.SubFraction(phanSo2);
+        phanSo1.MulFraction(phanSo2);
+        phanSo1.DivFraction(phanSo2);
+    }
 }
