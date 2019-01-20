ccbd02ca961e74856cbc22ae32e626b8538b5b9f
Congthanh9768 <vancong1166@gmail.com>
2019-01-20 09:55:16 +0700

    update

---
+++ Javacore2/src/bai89/SinXX.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai89;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Anonymous
+ */
+public class SinXX {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nhap do:  ");
+        double x = input.nextDouble(); // don v? là d?
+        // d?i don v? sang rad
+        double xRad = x * Math.PI / 180;
+        
+        double ERR = 0.0000001;
+        double dif = 0;
+        double sinx = 0;
+        int i = 0;
+        do {
+            dif = Math.pow(-1, i) * (Math.pow(xRad, 2*i+1) / factori(2*i+1));
+            sin += dif;
+            i++;
+        } while(Math.abs(dif) > ERR);
+        
+        System.out.println("===========================");
+        System.out.println("sin("+x+") = " + sin);
+    }
+    
+    public static long factori(long n) {
+        if (n == 0 || n == 1) return 1;
+        return n * factori(n - 1);
+    }
+    
+}
--- Javacore2/src/bai89/cosX.java
+++ Javacore2/src/bai89/cosX.java
@@ -1,40 +1,42 @@
-package bai89;
+﻿package bai89;
 
 import static java.lang.Math.pow;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 public class cosX {
 
-    public static double giaithua(double n){
-        double i,ketqua =1;
-        for(i = 2 ; i<=n; i++){
-            ketqua*=1;
-        }
-       return ketqua;
-    }
-    
-    
-     public static void main(String[] args) {
-         double ketqua, epsilon, x = 1, n =1,c;
-         
-     
-        Scanner scanner = new Scanner(System.in);
-         
-        System.out.printf("Mời bạn nhập vào số n: ");
-        n = scanner.nextInt();
-        System.out.printf("Mời bạn nhập vào số c: ");
-        c = scanner.nextInt();
-        ketqua = x;
+public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nhap do:  ");
+        double x = input.nextDouble(); // đơn vị là độ
+        // đổi đơn vị sang rad
+        double xRad = x * Math.PI / 180;
+        
+        double ERR = 0.0000001;
+        double dif = 0;
+        double cosx = 0;
+        int i = 0;
         do {
-            epsilon = pow(x, 2*n) / giaithua(2*n);
-            ketqua += pow(-1, n) * epsilon;
-            n++; 
-        } while (abs(epsilon) > c);
-         
-        System.out.printf("\nKet qua tinh theo cong thuc = %10.8lf",ketqua);
-      
+            dif = Math.pow(-1, i) * (Math.pow(xRad, 2*i) / factori(2*i));
+            cosx += dif;
+            i++;
+        } while(Math.abs(dif) > ERR);
+        
+        System.out.println("===========================");
+        System.out.println("cos("+x+") = " + cosx);
     }
-
-  
+    
+    public static int factori(int n) {
+        if (n == 0 || n == 1) return 1;
+        int gt = 1;
+        for (int i = 1; i <= n; i++) {
+            gt *= i;
+        }
+        return gt;
+    }
+    public static long factori(long n) {
+        if (n == 0 || n == 1) return 1;
+        return n * factori(n - 1);
+    }
     
 }
