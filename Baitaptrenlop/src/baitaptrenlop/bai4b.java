30b381fdcdf463e023438b4fa2be53abfdc56d7f
Congthanh9768 <vancong1166@gmail.com>
2019-02-12 16:56:06 +0700

    Bai tap buoi 4

--- Baitaptrenlop/src/baitaptrenlop/bai4b.java
+++ Baitaptrenlop/src/baitaptrenlop/bai4b.java
@@ -31,25 +31,35 @@ public class bai4b {
         return (n);
     }
     public static void main(String[] args) {
-        System.out.printf("Nhap: ");
-        int n =  nhap();
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=n-i;j++)
-                System.out.printf(" ");
-            for(int j=0;j<2*i+1;j++)
-                if(i==0 || i==(n-1)){
+        System.out.print("h = ");
+        int h = nhap();
+        for (int i = 0; i < h; i++) {
+            // in khoảng trắng
+            for (int k = 0; k < h - i - 1; k++) {
+                System.out.print(" ");
+            }
+            // in dấu *
+            for (int j = 0; j < 2*i+1; j++) {
+                // nếu là dòng đầu tiên hoac dòng cuối cùng
+                if ( i == 0 || i == (h-1) ) {
                     System.out.print("*");
-                }
-                else{
-                    if(j==0 || j==(2*i)){
-                        System.out.printf("*");
+                // nếu không 
+                } else {
+                    // --- nếu là vị trí đầu tiên hoặc vị trí cuối cùng thì
+                    if (j == 0 || j == (2*i) ) {
+                    // ---   in ra dấu *
+                        System.out.print("*");
+                    }// --- nếu không
+                    else {
+                    // ---   in ra khoảng trắng
+                        System.out.print(" ");
                     }
-                    else{
-                        System.out.printf(" ");
-                    }
                 }
-               System.out.println("");
-        } 
+            }
+            System.out.println("");
+        }
+    }
+    
     }
  
      
