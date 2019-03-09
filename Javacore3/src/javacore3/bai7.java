a1d71efe4c6cb34154f45bf849d0303c2dd7a1a8
Congthanh9768 <vancong1166@gmail.com>
2019-03-09 23:53:17 +0700

    javacore3 - lap4

--- Javacore3/src/javacore3/bai7.java
+++ Javacore3/src/javacore3/bai7.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+﻿
 package javacore3;
 
 import java.util.Scanner;
